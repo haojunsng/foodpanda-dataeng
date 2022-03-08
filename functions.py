fb9f4287c83d626e08e6f44362262b4d21543ea2
Hao Jun Sng <haojunsng@GomuMac.local>
2022-03-08 23:29:47 +0800

    Question 1

--- functions.py
+++ functions.py
@@ -9,11 +9,13 @@ def get_df():
     df = bqclient.list_rows('bigquery-public-data.geo_international_ports.world_port_index').to_dataframe()
     return df
 
-def write_df(dataframe, table):
+def write_df(dataframe, table_name):
     
     # Connecting to BigQuery Client using service account key (json) in config folder.
     bqclient = bigquery.Client.from_service_account_json('./config/foodpanda-assessment-343503-d4905b2cb42b.json')
     
+    dataset = bqclient.get_dataset('foodpanda-assessment-343503.foodpanda_tables')
+
     # Configuring the write_disposition for LoadJobConfig: WRITE_TRUNCATE --> overwrites the table if exists
     job_config = bigquery.LoadJobConfig(
         write_disposition=bigquery.job.WriteDisposition.WRITE_TRUNCATE
@@ -22,7 +24,7 @@ def write_df(dataframe, table):
     # insert table
     job = bqclient.load_table_from_dataframe(
         dataframe=dataframe,
-        destination=table,
+        destination=dataset.table(table_name),
         job_config=job_config
     )
     
